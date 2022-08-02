5436b83bbd244e514c01d3210223ff63d005b2f1
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-08-02 16:00:01 -0400

    adding viewemployees and addemployees function to index.js file

--- index.js
+++ index.js
@@ -11,10 +11,36 @@ const menuQuestion=[
         name:"menu",
         message:"choose the following options:",
         choices:["view all departments"," view all roles","view all employees","add a department","adda role","add an employee","update an employee role"]
-        
+
     }
 ]
-
+const employeeAddQuestions=[
+    {
+        type:"input",
+        name:"first_name",
+        message:"what is your first name?",
+    },
+    {
+        type:"input",
+        name:"last_name",
+        message:"what is your last name?",
+    }
+]
 function menu(){
+    inquirer.prompt(menuQuestion)
+    .then(response=>{
+        if(response.menu==="view all employees"){
+            viewEmployees()
+        }
+        else if(response.menu==="add an employee"){
+                addEmployees()
+        }
+    })
+}
+
+function viewEmployees(){
+    db.query
+}
+function addEmployees(){
 
 }
